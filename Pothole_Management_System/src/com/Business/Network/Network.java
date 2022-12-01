33615a4c162490a0b4803dda6d34669f00f60ef8
Shardul Surve <113263274+shardulsurve1@users.noreply.github.com>
2022-12-01 14:39:20 -0500

    Added Network class to Business

---
+++ Pothole_Management_System/src/com/Business/Network/Network.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Business.Network;
+
+import com.Business.Enterprise.EnterpriseDir;
+
+//network
+public class Network {
+    private String networkName;
+    private EnterpriseDir enterpriseDir;
+    
+    public Network()
+    {
+     enterpriseDir = new EnterpriseDir();
+    }
+    
+    //enterprise directory
+
+    
+}
+
