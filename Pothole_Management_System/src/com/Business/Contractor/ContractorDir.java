83d7344c3d1e7e5e88f5030a0c1542a2cca13635
Ganesh Bhagwat Patil <patil.gan@northeastern.edu>
2022-12-01 16:20:30 -0500

    Added contractor directory

---
+++ Pothole_Management_System/src/com/Business/Contractor/ContractorDir.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Business.Contractor;
+
+import java.util.ArrayList;
+
+
+public class ContractorDir {
+    private ArrayList<Contractor> ngoDirectory;
+
+    public ContractorDir() {
+    ngoDirectory= new ArrayList<>();
+    }
+    
+}
