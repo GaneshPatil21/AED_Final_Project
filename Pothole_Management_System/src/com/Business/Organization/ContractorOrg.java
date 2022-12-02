e66a80d2aaeeff83351420d021802e71152681e6
Shardul Surve <113263274+shardulsurve1@users.noreply.github.com>
2022-12-02 15:13:38 -0500

    Added contractor Organization

---
+++ Pothole_Management_System/src/com/Business/Organization/ContractorOrg.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Business.Organization;
+
+import com.Business.Contractor.ContractorDir;
+import com.Business.Role.ContractorManagerRole;
+import com.Business.Role.Role;
+import java.util.HashSet;
+
+
+public class ContractorOrg extends Organization{
+    ContractorDir ngoList;
+    //constructor to add NGO list to NGO directory
+    public ContractorOrg()
+    {
+        super(Organization.Type.NGO.getValue());
+        ngoList=new ContractorDir();
+    }
+
+    
+
+}
