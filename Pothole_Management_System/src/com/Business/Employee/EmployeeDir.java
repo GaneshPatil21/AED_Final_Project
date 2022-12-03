a53059e5f89279f476c35c13e1cb3bcd09b5d339
Ganesh Bhagwat Patil <patil.gan@northeastern.edu>
2022-12-02 19:02:59 -0500

    Added Employee Directory

---
+++ Pothole_Management_System/src/com/Business/Employee/EmployeeDir.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Business.Employee;
+
+import java.util.ArrayList;
+
+
+public class EmployeeDir {
+    private ArrayList<Employee> empList;
+    
+    //create employeeDir
+    
+    public EmployeeDir(){
+        empList=new ArrayList<>();
+    }
+
+   
+}
