1b6d440848484abb0ca314fe474a0bf783dbdc61
Ganesh Bhagwat Patil <patil.gan@northeastern.edu>
2022-12-05 10:09:00 -0500

    Created Police enterprise class

---
+++ Pothole_Management_System/src/com/Business/Enterprise/PoliceEnt.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Business.Enterprise;
+
+import com.Business.Role.PoliceRole;
+import com.Business.Role.Role;
+import java.util.HashSet;
+
+//policeenterprise
+public class PoliceEnt extends Enterprise{
+    public PoliceEnt(String name){
+        super(name,Enterprise.EnterpriseType.Police);
+    }
+
+}
