5e6f64f5d3c64ea59d8a5ad589f4e566d619d935
Ganesh Bhagwat Patil <patil.gan@northeastern.edu>
2022-12-05 18:54:18 -0500

    Created Road Safety enterprise

---
+++ Pothole_Management_System/src/com/Business/Enterprise/RoadSafetyEnt.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Business.Enterprise;
+
+import com.Business.Role.RoadSafetyRole;
+import com.Business.Role.Role;
+import java.util.HashSet;
+
+//Hospital enterprise
+public class RoadSafetyEnt extends Enterprise{
+    public RoadSafetyEnt(String name){
+        super(name,Enterprise.EnterpriseType.Hospital);
+    }
+    
+
+}
