58f93fceb562d3abf193d3e7f3461e3fe736f75d
Ganesh Bhagwat Patil <patil.gan@northeastern.edu>
2022-12-06 14:26:30 -0500

    Implemeted fireman directory

---
+++ Pothole_Management_System/src/com/Business/FireMan/FireManDir.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Business.FireMan;
+
+import java.util.ArrayList;
+//fireman directory
+public class FireManDir {
+
+    private ArrayList<FireMan> fireManList;
+
+    public FireManDir() {
+        fireManList = new ArrayList();
+
+    }
+
+}
